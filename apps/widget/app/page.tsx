67c874998171fef148d030e7486a079daf378c24
Minh Tran <trcongminh.261002@gmail.com>
2025-08-08 18:01:52 +0700

    chore: update dependencies, build config, and enhance UI components
    - Updated pnpm-lock.yaml with new dependencies and devDependencies
    - Modified turbo.json to include dist/** in outputs
    - Adjusted web app package.json dev command and added workspace dependencies
    - Enhanced page.tsx with new components and functionality
    - Expanded ui/package.json with additional Radix UI components and supporting libraries

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,13 @@
-import { Button } from "@workspace/ui/components/button"
 import { add } from "@workspace/math/add"
-import { Input } from "@workspace/ui/components/input";
+import { Button } from "@workspace/ui/components/button"
 
 export default function Page() {
-  console.log(add(2, 2));
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello From app/web</h1>
-        <Input />
+        <h1 className="text-2xl font-bold">Hello From app/widget</h1>
         <Button size="sm">Button</Button>
+        {add(2, 2)}
       </div>
     </div>
   )
